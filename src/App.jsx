411708ce60bb0a5243a14396de56b1e41d3a1000
Prince Kumar <63802036+princek18@users.noreply.github.com>
2023-10-22 19:50:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
 import { MainComponent } from "./Components/MainComponent";
 
-export const baseURL = "http://localhost:4000/";
+// export const baseURL = "http://localhost:4000/";
+
+export const baseURL = "https://parshav-be.onrender.com/";
 
 function App() {
   return (
